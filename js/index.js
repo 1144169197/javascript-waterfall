30673b9fa8d7e520f0c8a814ea391220d31ae152
wulei <1144169197@qq.com>
2018-11-21 17:06:27 +0800

    【javascript瀑布流】提交

---
+++ js/index.js
@@ -0,0 +1,21 @@
+﻿/*********************************************************************************
+Name:瀑布流
+Author:ll
+Date:2018
+Desc:操作
+version:1.0
+**********************************************************************************/
+function waterfall(id, col, gap) {
+	this._id = id || 'container';
+	this._col = col || 4;
+	this._gap = gap || [10, 10];
+}
+
+/*
+描述：初始化
+参数：
+conatiner: 初始化
+*/
+waterfall.prototype.init = function (container) {
+	
+}
